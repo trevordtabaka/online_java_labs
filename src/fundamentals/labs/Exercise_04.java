3dee3d0114f11ab541c27280d7fd7a0ab06cef82
Trevor <tta@universal-robots.com>
2018-02-04 17:47:09 +0100

    Added necessary code to complete exercise 04

--- src/fundamentals/labs/Exercise_04.java
+++ src/fundamentals/labs/Exercise_04.java
@@ -12,7 +12,17 @@ public class Exercise_04 {
 
     public static void main(String[] args) {
 
-        // write code here
+
+            double radius = 3.14;
+            double height = 5;
+
+            double perimeter = 2*Math.PI*radius;
+            double area = perimeter*(radius+height);
+
+            System.out.println("Perimeter =: " + perimeter);
+            System.out.println("Area =: " + area);
+
+
 
     }
 }

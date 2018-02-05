e96c258796853f4042422b4af1a177607942e221
Trevor <tta@universal-robots.com>
2018-02-05 17:47:53 +0100

    Added necessary code to complete exercise 01 in data types and operators

--- src/datatypes_operators/labs/Exercise_01.java
+++ src/datatypes_operators/labs/Exercise_01.java
@@ -16,7 +16,18 @@ public class Exercise_01 {
 
     public static void main(String[] args) {
 
-        // write code here
+        int integer = 5;
+        double dubble = 333333.123456;
+        float flote = 12.12345f;
+        char character = 'g';
+        char char2 = '7';
+
+
+        System.out.println(integer);
+        System.out.println(dubble);
+        System.out.println(flote);
+        System.out.println(character);
+        System.out.println(char2);
 
     }
 }

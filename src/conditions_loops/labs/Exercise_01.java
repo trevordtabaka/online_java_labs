5f2d6d255f75c7e5e162c1ec14cfe74367c80a2d
Trevor <tta@universal-robots.com>
2018-02-06 19:02:44 +0100

    conditions_loops Exercise 02 completed

--- src/conditions_loops/labs/Exercise_01.java
+++ src/conditions_loops/labs/Exercise_01.java
@@ -23,7 +23,16 @@ public class Exercise_01 {
         // assign input to variable as int
         int number = scanner.nextInt();
 
-        // write completed code here
+        if(number >0 && number < 1000000000){
+            if(number % 2 == 1) {
+                System.out.println(number + " is an odd number");
+            }else{
+                System.out.println(number + " is an even number");
+
+            }
+        } else{
+            System.out.println("You Lose. Read directions next time");
+        }
 
     }
 }

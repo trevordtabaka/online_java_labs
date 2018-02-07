36b9ac2c479a421606817edf81bcbe1aa8967de3
Trevor <tta@universal-robots.com>
2018-02-07 17:55:04 +0100

    Completed conditions_loops exercise 2

--- src/conditions_loops/labs/Exercise_02.java
+++ src/conditions_loops/labs/Exercise_02.java
@@ -1,5 +1,6 @@
 package conditions_loops.labs;
 
+import java.nio.channels.Pipe;
 import java.util.Scanner;
 
 /**
@@ -15,13 +16,30 @@ public class Exercise_02 {
     public static void main(String[] args) {
 
         // create scanner
-
+        Scanner scanner = new Scanner(System.in);
         // prompt user
+        System.out.println("Enter a number 1-7");
 
         // assign input to variable as int
-
+        int input = scanner.nextInt();
 
         // write completed code here
-
+        if(input == 1){
+            System.out.println("Monday");
+        }else if(input == 2){
+            System.out.println("Tuesday");
+        }else if(input == 3){
+            System.out.println("Wednesday");
+        }else if(input == 4){
+            System.out.println("Thursday");
+        }else if(input == 5){
+            System.out.println("Friday");
+        }else if(input == 6){
+            System.out.println("Saturday");
+        }else if(input == 7){
+            System.out.println("Sunday");
+        }else{
+            System.out.println("Other");
+        }
     }
 }

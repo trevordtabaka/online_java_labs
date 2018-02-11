2bd86731759534221a1bdd2a783f8bc1c282603d
Trevor <tta@universal-robots.com>
2018-02-11 18:08:50 +0100

    Completed datatypes_operators exercise 6

--- src/datatypes_operators/labs/Exercise_06.java
+++ src/datatypes_operators/labs/Exercise_06.java
@@ -20,5 +20,27 @@ public class Exercise_06 {
     public static void main(String[] args) {
 
         int[][] twoDArray = new int[5][5];
+        int number = 0;
+        
+        for(int i = 0; i<twoDArray.length; i++){
+
+            for(int j = 0; j<twoDArray[i].length; j++){
+
+                number = number + 3;
+                twoDArray[i][j] = number;
+
+
+            }
+        }
+        for(int i = 0; i<twoDArray.length; i++){
+
+            for(int j = 0; j<twoDArray[i].length; j++){
+
+                System.out.print(twoDArray[i][j] + " ");
+
+            }
+            System.out.println();
+        }
+
     }
 }

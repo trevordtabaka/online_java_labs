fcb59498efaf1966b3d233825ad12bda2759c602
Trevor <tta@universal-robots.com>
2018-02-11 17:51:59 +0100

    updated datatypes_operators questions and completed exercise 4 for it as well

--- src/datatypes_operators/labs/Exercise_04.java
+++ src/datatypes_operators/labs/Exercise_04.java
@@ -1,5 +1,9 @@
 package datatypes_operators.labs;
 
+
+import java.util.Scanner;
+import java.util.Arrays;
+
 /**
  * Data Types and Operators Exercise 4: Arrays calculator
  *
@@ -10,6 +14,50 @@ package datatypes_operators.labs;
  *
  */
 
-public class Exercise_04{
+ public class Exercise_04 {
+    static int count = 0;
+    static int[] numberArray = new int[10];
+
+    public static void main(String[] args) {
+
+        // Get user input and into an array
+        userInput();
+
+        calcSumAvg(numberArray);
+
+
+
+
+
+
+
+
+    }
+
+    public static void userInput(){
+
+        for(int i = count; i<10;i++) {
+            System.out.println("Input a number. You have inputted " + (i) + "/10 numbers so far");
+            Scanner scanner = new Scanner(System.in);
+            int input = scanner.nextInt();
+            numberArray[i] = input;
+
+            count++;
+        }
+    }
+    public static void calcSumAvg(int[] numberArray){
+        double sum=0;
+        double avg;
+        for(int i= 0; i<numberArray.length; i++){
+           // System.out.println("before add: " + sum);
+
+            sum = sum + numberArray[i];
+          //  System.out.println("after add: " + sum);
+        }
+        avg = sum / 10;
+        System.out.println("The sum of the numbers is: " + sum);
+        System.out.println("The average of the numbers is: " + avg);
+
+    }
 
 }

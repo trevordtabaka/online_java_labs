d05b5e6b2b0a88b6bb6dddfa203329719f0afe29
Trevor <tta@universal-robots.com>
2018-02-11 18:00:30 +0100

    Completed datatypes_operators exercise 5

--- src/datatypes_operators/labs/Exercise_05.java
+++ src/datatypes_operators/labs/Exercise_05.java
@@ -1,5 +1,7 @@
 package datatypes_operators.labs;
 
+import java.util.Scanner;
+
 /**
  * Data Types and Operators Exercise 5: More arrays
  *
@@ -13,6 +15,22 @@ public class Exercise_05{
     public static void main(String[] args) {
 
         int[] array = {10, 9, 8, 7, 6, 5, 4, 3, 2, 1};
+
+        System.out.println("Enter a number 1-10.");
+        Scanner scanner = new Scanner(System.in);
+        int number = scanner.nextInt();
+
+        for(int i: array){
+
+            if(i == number){
+
+                System.out.println(number + " is located in index " + array[i]);
+            }
+
+        }
+
+
+
 
         // write code here
 
